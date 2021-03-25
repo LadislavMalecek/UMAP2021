116f2aa35262e2d9517e414955f2d9aa01f3c67f
Ladislav Malecek <malecek.ladislav@gmail.com>
2021-03-25 12:49:10 +0100

    reorganising

---
+++ java/run_ml1m_longterm.sh
@@ -0,0 +1,12 @@
+mvn clean compile;
+export MAVEN_OPTS="-Xmx11000M"
+
+
+for g_s in 2 3 4 8
+do
+    for g in sim div
+    do
+        mvn exec:java -Dexec.mainClass="gfar.RunDDOAInTime" \
+            -Dexec.args="--data=ml1m --groupType=${g} --groupSize=${g_s}"
+    done
+done
---
+++ java/run_ml1m_weighted.sh
@@ -0,0 +1,19 @@
+mvn clean compile;
+export MAVEN_OPTS="-Xmx11000M"
+
+
+# before running this
+# - generate results of standard non-weighted script
+# - generate weights using python script
+
+for g_s in 2 3 4 8
+do
+    for g in sim
+    do
+        mvn exec:java -Dexec.mainClass="gfar.AggregateRecommedations" \
+            -Dexec.args="--data=ml1m --groupType=${g} --groupSize=${g_s} --userPref"
+
+        mvn exec:java -Dexec.mainClass="gfar.RunDDOA" \
+            -Dexec.args="--data=ml1m --groupType=${g} --groupSize=${g_s} --userPref"
+    done
+done
